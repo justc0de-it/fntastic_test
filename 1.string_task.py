a5733280dfa46f3a7f4c3323237fbed9a6b94562
bantikgames <anishenko.alex@gmail.com>
2022-08-02 02:25:47 +0500

    Completed the first part of the test task

---
+++ 1.string_task.py
@@ -0,0 +1,22 @@
+# Since strings are immutable objects, we create an empty list to store data for a new string there.
+new_list = []
+
+
+def convert_string(string):
+    # We cycle through the line to find out the number of repetitions of letters
+    for symbol in string:
+        # If the letter is repeated once, then insert it into the list (
+        if string.count(symbol) == 1:
+            new_list.append("(")
+        # If the letter is repeated several times, then we insert it into the list )
+        else:
+            new_list.append(")")
+    # Turning the contents of the list into a string
+    new_str = "".join(new_list)
+    # Output the resulting string
+    print(new_str)
+
+
+if __name__ == "__main__":
+    input_user_string = input("Enter the text: ")
+    convert_string(input_user_string)
